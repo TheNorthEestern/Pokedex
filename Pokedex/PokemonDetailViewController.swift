958587ab51dceb279a083ba7d68f5add9a7bfcce
Kacy James <inquen@gmail.com>
2016-12-07 00:47:55 -0500

    Added a variety of improvements

--- Pokedex/PokemonDetailViewController.swift
+++ Pokedex/PokemonDetailViewController.swift
@@ -10,19 +10,25 @@ import UIKit
 
 class PokemonDetailViewController: UIViewController {
 
+    @IBOutlet weak var pokemonImage: UIImageView!
     @IBOutlet weak var nameLabel: CLTypingLabel!
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setNeedsStatusBarAppearanceUpdate()
-        nameLabel.text = pokemon.name
+        nameLabel.text = pokemon.name.capitalized
+        pokemonImage.image = UIImage(named: "\(pokemon.pokedexId)")
+
         // Do any additional setup after loading the view.
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .default
     }
-
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+      dismiss(animated: true, completion: nil)
+    }
 
 }
